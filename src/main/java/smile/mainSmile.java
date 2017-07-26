e4d9ff7b969614173dbb7a56367992f290b7b21a
tantikristanti <tantikristanti@gmail.com>
2017-07-26 17:14:21 +0200

    Initial commit

--- src/main/java/smile/mainSmile.java
+++ src/main/java/smile/mainSmile.java
@@ -6,11 +6,15 @@ import java.io.InputStreamReader;
 
 public class mainSmile {
     public static void main(String[] args) throws Exception {
+        BufferedReader br = null;
+
         smileUsage sm = new smileUsage();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("Name of training data file : ");
         File file_name = new File(br.readLine());
         sm.loadData(file_name);
     }
+
+
 }
--- src/main/java/smile/smileUsage.java
+++ src/main/java/smile/smileUsage.java
@@ -2,10 +2,12 @@ package smile;
 
 import smile.data.AttributeDataset;
 import smile.data.parser.ArffParser;
-import smile.regression.RandomForest;
+import smile.classification.*;
+import smile.math.Math;
+import smile.validation.*;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.*;
+
 
 
 /* this class contains the use of Smile Java API for predictive modeling */
@@ -14,28 +16,84 @@ public class smileUsage {
     // --------------- attributes ---------------
     private ArffParser arffParser = null;
     private AttributeDataset attributeDataset = null;
+    private AttributeDataset training = null;
+    private AttributeDataset testing = null;
     private RandomForest forest = null;
 
     /* --------------- methods ---------------
 
-     load the training data */
+     loading only training data */
     public void loadData(File file) throws Exception {
+        // setResponseIndex is response variable; for classification, it is the class label; for regression, it is of real value
         arffParser = new ArffParser().setResponseIndex(4);
+        // dataset of a number of attributes
         attributeDataset = arffParser.parse(new FileInputStream(file));
-        double[][] x = attributeDataset.toArray(new double[attributeDataset.size()][]);
-        int[] y = attributeDataset.toArray(new int[attributeDataset.size()]);
 
+        System.out.println("Loaded training data: " + file.getPath());
+    }
+
+
+    // loading training and testing data
+    public void loadDataTrainTest(File file1, File file2) throws Exception {
+        // setResponseIndex is response variable; for classification, it is the class label; for regression, it is of real value
+        arffParser = new ArffParser().setResponseIndex(4);
+        // dataset of a number of attributes
+        training = arffParser.parse(new FileInputStream(file1));
+        testing = arffParser.parse(new FileInputStream(file2));
+
+        System.out.println("Loaded training data : " + file1.getPath());
+        System.out.println("Loaded testing data : " + file1.getPath());
     }
 
     // training the model with Random Forest
     public void trainModel() throws Exception {
         double[][] x = attributeDataset.toArray(new double[attributeDataset.size()][]);
-        double[] y = attributeDataset.toArray(new double[attributeDataset.size()]);
+        int[] y = attributeDataset.toArray(new int[attributeDataset.size()]);
 
-        // maximum number of trees: 200; maximum number of leaves:4
-        forest = new RandomForest(attributeDataset.attributes(), x, y, 200,4 );
+        // maximum number of trees: 200
+        forest = new RandomForest(attributeDataset.attributes(), x, y, 200);
     }
 
-    // validating the model
+    /* validation model using LOOCV (leave-one-out cross validation);
+        if it has only a single dataset for building models
+     */
+    public void validationModelLOOCV() throws Exception{
+        double[][] x = attributeDataset.toArray(new double[attributeDataset.size()][]);
+        int[] y = attributeDataset.toArray(new int[attributeDataset.size()]);
 
+        int leng = x.length;
+        LOOCV loocv = new LOOCV(leng);
+
+        int count_error = 0;
+        for (int i=0; i<leng; i++){
+            double[][] trainx = Math.slice(x, loocv.train[i]);
+            int[] trainy = Math.slice(y, loocv.train[i]);
+
+            forest = new RandomForest(attributeDataset.attributes(), trainx, trainy, 200);
+
+            if (y[loocv.test[i]] != forest.predict(x[loocv.test[i]]))
+                count_error++;
+        }
+        System.out.println("Number of error: "+ count_error);
+        System.out.format("Error rate : %.2f%%\n "+ 100.0 * count_error / leng );
+    }
+
+    // validation model using training and testing data
+    public void validationModel() throws Exception{
+        double[][] trainx = training.toArray(new double[training.size()][]);
+        int[] trainy = training.toArray(new int[training.size()]);
+        double[][] testx = testing.toArray(new double[testing.size()][]);
+        int[] testy = testing.toArray(new int[testing.size()]);
+
+        forest = new RandomForest(attributeDataset.attributes(), trainx, trainy,200);
+
+        int count_error = 0;
+        for (int i = 0; i < testx.length; i++) {
+            if (forest.predict(testx[i]) != testy[i]) {
+                count_error++;
+            }
+        }
+        System.out.println("Number of error: "+ count_error);
+        System.out.format("Error rate : %.2f%%\n "+ 100.0 * count_error / testx.length );
+    }
 }
