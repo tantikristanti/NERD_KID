431e3d31d2f7734399bfd3db55698625be18e60f
tantikristanti <tantikristanti@gmail.com>
2017-08-09 16:47:47 +0200

    Initial commit

---
+++ src/main/java/restAPI/callRestAPI.java
@@ -0,0 +1,33 @@
+package restAPI;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.*;
+
+public class callRestAPI {
+    public void callRestService(String link, String query) throws Exception{
+        URL url = new URL(link);
+
+        // making a connection
+        URLConnection urlConnection = url.openConnection();
+
+        // setDoOutput : POST, setDoInput : GET
+        urlConnection.setDoOutput(true);
+        urlConnection.setAllowUserInteraction(false);
+
+        // sending a query
+        PrintStream printStream = new PrintStream(urlConnection.getOutputStream());
+        printStream.print(query);
+        printStream.close();
+
+        // getting the result
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+        String result = null;
+        while ((result = bufferedReader.readLine()) != null)
+            System.out.println(result);
+
+        bufferedReader.close();
+    }
+}
---
+++ src/main/java/restAPI/mainRestService.java
@@ -0,0 +1,7 @@
+package restAPI;
+
+public class mainRestService {
+    public static void main(String[] args) {
+
+    }
+}
