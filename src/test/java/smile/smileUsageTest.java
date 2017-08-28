c518675fcf30476d7a3a3c2a7096acc120f88536
tantikristanti <tantikristanti@gmail.com>
2017-08-28 16:30:15 +0200

    checking error for deployment

--- src/test/java/smile/smileUsageTest.java
+++ src/test/java/smile/smileUsageTest.java
@@ -4,7 +4,7 @@ public class smileUsageTest {
     @org.junit.Test
     public void loadData() throws Exception {
         SmileUsage sm = new SmileUsage();
-        sm.loadData(new java.io.File("data/model7/Model7_P21_P31_P106_P361_P373_P569_P625_P646_P910_RI334.arff"));
+        sm.loadData(new java.io.File("data/model7/Model7_P17_P21_P31_P106_P159_P361_P373_P569_P625_P646_P910_RI337.arff"));
     }
 
 }
