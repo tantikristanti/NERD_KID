1a40d112471b213af79e75ad6e4de6cce2c31ee3
Luca Foppiano <luca@foppiano.org>
2017-11-27 17:33:08 +0100

    making this test more meaningful

--- src/test/java/org/nerd/kid/model/PredictorTest.java
+++ src/test/java/org/nerd/kid/model/PredictorTest.java
@@ -4,8 +4,12 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class PredictorTest {
     WikidataNERPredictor target;
+
     @Before
     public void setUp() throws Exception {
         target = new WikidataNERPredictor();
@@ -15,24 +19,24 @@ public class PredictorTest {
     @Test
     @Ignore("Not a unit test")
     public void predict() throws Exception {
-        System.out.println(target.predict("Q490").getPredictedClass());
+        assertThat(target.predict("Q490").getPredictedClass(), is("LOCATION"));
 
-        System.out.println(target.predict("Q12345").getPredictedClass());
-        System.out.println(target.predict("Q1234").getPredictedClass());
-        System.out.println(target.predict("Q55555").getPredictedClass());
-        System.out.println(target.predict("Q666").getPredictedClass());
+        assertThat(target.predict("Q12345").getPredictedClass(), is("PERSON"));
+        assertThat(target.predict("Q1234").getPredictedClass(), is("LOCATION"));
+        assertThat(target.predict("Q55555").getPredictedClass(), is("LOCATION"));
+        assertThat(target.predict("Q666").getPredictedClass(), is("LOCATION"));
 
         //ibm
-        System.out.println(target.predict("Q202712").getPredictedClass());
+        assertThat(target.predict("Q202712").getPredictedClass(), is("LOCATION"));
 
         //apple
-        System.out.println(target.predict("Q312").getPredictedClass());
+        assertThat(target.predict("Q312").getPredictedClass(), is("LOCATION"));
 
         //charlemagne
-        System.out.println(target.predict("Q3044").getPredictedClass());
+        assertThat(target.predict("Q3044").getPredictedClass(), is("PERSON"));
 
         //11th century
-        System.out.println(target.predict("Q7063").getPredictedClass());
+        assertThat(target.predict("Q7063").getPredictedClass(), is("LOCATION"));
 
     }
 
