ce9a43e28b0e628e5d4ae14f24ad27ae9a919244
tantikristanti <tantikristanti@gmail.com>
2018-01-02 15:29:41 +0100

    correct some bugs in a class 'WikibaseWrapper' and a pom file

--- src/main/java/org/nerd/kid/extractor/wikidata/WikibaseWrapper.java
+++ src/main/java/org/nerd/kid/extractor/wikidata/WikibaseWrapper.java
@@ -17,6 +17,7 @@ public class WikibaseWrapper implements WikidataFetcherWrapper {
     FeatureFileExtractor featureFileExtractor = new FeatureFileExtractor();
 
     public WikidataElement getElement(String wikiId) throws Exception {
+        String labelItem = null;
 
         System.out.println("Fetch data from wikidata " + wikiId);
 
@@ -26,50 +27,54 @@ public class WikibaseWrapper implements WikidataFetcherWrapper {
 
         ItemDocument document = (ItemDocument) wikibaseDataFetcher.getEntityDocument(wikiId);
 
-        if (document == null) {
-            System.out.println("Data couldn't be fetched.");
-        }
+        if (document != null) {
 
-        // get the label of wikidata Id
-        String labelItem = document.getLabels().get("en").getText();
-        if (labelItem.equals(null)){
-            labelItem = "Null";
-        }
+            // get the label of wikidata Id
+            labelItem = document.getLabels().get("en").getText();
+            if (labelItem.equals(null)) {
+                labelItem = "Null";
 
-        // list for storing properties and its values
-        //Map<String, List<String>> dataPropertyValue = featureFileExtractor.loadFeatures();
-        Map<String, List<String>> dataPropertyValue = new HashMap<>();
-        for (StatementGroup statementGroup : document.getStatementGroups()) {
+                // list for storing properties and its values
+                //Map<String, List<String>> dataPropertyValue = featureFileExtractor.loadFeatures();
+                Map<String, List<String>> dataPropertyValue = new HashMap<>();
+                for (StatementGroup statementGroup : document.getStatementGroups()) {
 
-            // get the properties
-            String property = statementGroup.getProperty().getId().toString().trim();
+                    // get the properties
+                    String property = statementGroup.getProperty().getId().toString().trim();
 
-            // get the values
-            for (Statement statement : statementGroup) {
-                if (statement.getClaim().getMainSnak() instanceof ValueSnak) {
-                    Value value = (statement.getClaim().getMainSnak()).getValue();
-                    if (value instanceof ItemIdValue) {
+                    // get the values
+                    for (Statement statement : statementGroup) {
+                        if (statement.getClaim().getMainSnak() instanceof ValueSnak) {
+                            Value value = (statement.getClaim().getMainSnak()).getValue();
+                            if (value instanceof ItemIdValue) {
 
-                        // so we have property:property; value:valueofProperty
-                        String valueOfProperty = ((ItemIdValue) value).getId().toString().trim();
+                                // so we have property:property; value:valueofProperty
+                                String valueOfProperty = ((ItemIdValue) value).getId().toString().trim();
 
-                        //if the property has not been added in the map, create a new property-values
-                        if (dataPropertyValue.get(property) == null) {
-                            List<String> values = new ArrayList<>();
-                            values.add(valueOfProperty);
-                            dataPropertyValue.put(property, values);
-                        } else {
-                            //if the property exists in the map,just add the values with the same property
-                            dataPropertyValue.get(property).add(valueOfProperty);
+                                //if the property has not been added in the map, create a new property-values
+                                if (dataPropertyValue.get(property) == null) {
+                                    List<String> values = new ArrayList<>();
+                                    values.add(valueOfProperty);
+                                    dataPropertyValue.put(property, values);
+                                } else {
+                                    //if the property exists in the map,just add the values with the same property
+                                    dataPropertyValue.get(property).add(valueOfProperty);
+                                }
+                            }
                         }
                     }
                 }
+
+                wikidataElement.setId(wikiId);
+                wikidataElement.setLabel(labelItem);
+                wikidataElement.setProperties(dataPropertyValue);
+
             }
+        } else {
+            System.out.println("Data couldn't be fetched.");
         }
 
-        wikidataElement.setId(wikiId);
-        wikidataElement.setLabel(labelItem);
-        wikidataElement.setProperties(dataPropertyValue);
         return wikidataElement;
-    }
-}
+    } // end of method
+} // end of class
+
